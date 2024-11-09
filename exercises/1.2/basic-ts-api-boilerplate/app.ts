7490e6ed98c041cd6c0a2748bbadb61ef8f680ec
yuhang-wu-vinci <yuhang.wu@student.vinci.be>
2024-11-09 12:39:46 +0100

    new: ex1.2

--- exercises/1.2/basic-ts-api-boilerplate/app.ts
+++ exercises/1.2/basic-ts-api-boilerplate/app.ts
@@ -7,6 +7,21 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+let getRequestCount = 0;
+
+// Middleware pour enregistrer et afficher les statistiques des requêtes GET
+app.use((req, _res, next) => {
+    if (req.method === 'GET') {
+        getRequestCount++;
+        console.log(`GET counter : ${getRequestCount}`);
+    }
+    next();
+});
+
+
+
+
 app.use("/films", pizzaRouter);
 
 export default app;
